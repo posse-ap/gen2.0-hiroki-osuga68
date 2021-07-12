267148daa041d1cb706e3480d8177496ca939315
hiroki-osuga68 <osugahi1006@keio.jp>
2021-07-13 01:17:19 +0900

    7/12 problem-solved

--- Quizy assignment/quizy1.js
+++ Quizy assignment/quizy1.js
@@ -1,27 +1,24 @@
 let buttonTrue = document.getElementById("1-choice1");
-let buttonFalse1 =document.getElementById('1-choice2');
-let buttonFalse2 =document.getElementById('1-choice3');
+let buttonFalse1 = document.getElementById('1-choice2');
+let buttonFalse2 = document.getElementById('1-choice3');
 let resultDivided = document.getElementById('result-area');
 
 
-//正解の時の結果
+//正解の時の結果...こちらはaddEventListenerでイベント指定してみた
 function correctAnswer(){
    buttonTrue.style.color = '#ffffff'; 
     buttonTrue.classList.add('background-color');
 
- //正解エリアの作成
+ //正解エリアの作成 
 
    let explain = document.createElement('p');
     explain.classList.add("underline");
     explain.innerText = '正解！';
     resultDivided.appendChild(explain);
 
-    console.log(explain);
-
     let explain2 = document.createElement('p');
     explain2.innerText = '正解は「たかなわ」です';
     resultDivided.appendChild(explain2);
-
 
     resultDivided.classList.add('result-border');
     
@@ -34,7 +31,7 @@ function correctAnswer(){
 buttonTrue.addEventListener('click', correctAnswer);
 
 
-//不正解の時の結果
+//不正解の時の結果１...不正解のほうはonclickを用いて関数を起動させてみた
 buttonFalse1.onclick = function (){
 
     buttonTrue.style.color = '#ffffff'; 
@@ -43,19 +40,26 @@ buttonFalse1.onclick = function (){
     buttonFalse1.style.color = '#ffffff'; 
     buttonFalse1.classList.add('background-color2');
    
-//不正解エリアの作成
+//不正解エリアの作成１
 
     
     let failure1 = document.createElement('span');
     failure1.className = 'underline2';
     failure1.innerText = '不正解（泣）';
     resultDivided.appendChild(failure1);
+
+    let explain2 = document.createElement('p');
+    explain2.innerText = '正解は「たかなわ」です';
+    resultDivided.appendChild(explain2);
+
+    resultDivided.classList.add('result-border');
+    
     buttonTrue.classList.add("cannot-click");
     buttonFalse1.classList.add("cannot-click");
     buttonFalse2.classList.add("cannot-click");
     
 }
-//不正解の時の結果
+//不正解の時の結果２...不正解のほうはonclickを用いて関数を起動させてみた
 buttonFalse2.onclick = function (){
 
     buttonTrue.style.color = '#ffffff'; 
@@ -63,11 +67,20 @@ buttonFalse2.onclick = function (){
 
     buttonFalse2.style.color = '#ffffff';
     buttonFalse2.classList.add('background-color2');
-//不正解エリアの作成
+
+//不正解エリアの作成２
+
     let failure2 = document.createElement('span');
     failure2.className = 'underline2';
     failure2.innerText = '不正解（泣）';
     resultDivided.appendChild(failure2);
+
+    let explain2 = document.createElement('p');
+    explain2.innerText = '正解は「たかなわ」です';
+    resultDivided.appendChild(explain2);
+
+    resultDivided.classList.add('result-border');
+    
     buttonTrue.classList.add("cannot-click");
     buttonFalse1.classList.add("cannot-click");
     buttonFalse2.classList.add("cannot-click");
