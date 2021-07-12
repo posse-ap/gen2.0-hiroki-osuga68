0dba277964fe0cbe2b26987ef5bd2c52e9ba0ff3
hiroki-osuga68 <osugahi1006@keio.jp>
2021-07-12 22:28:41 +0900

    7/12 question

--- Quizy assignment/quizy1.js
+++ Quizy assignment/quizy1.js
@@ -1,7 +1,7 @@
 let buttonTrue = document.getElementById("1-choice1");
 let buttonFalse1 =document.getElementById('1-choice2');
 let buttonFalse2 =document.getElementById('1-choice3');
-const resultDivided = document.getElementById('result-area');
+let resultDivided = document.getElementById('result-area');
 
 
 //正解の時の結果
@@ -9,32 +9,30 @@ function correctAnswer(){
    buttonTrue.style.color = '#ffffff'; 
     buttonTrue.classList.add('background-color');
 
-    //正解エリアの作成
-    
-    let result = document.createElement('span');
-    result.className = 'underline';
-    result.innerText = '正解!';
-    resultDivided.appendChild(result);
+ //正解エリアの作成
 
-    let explain = document.createElement('p');
-    explain.innerText = '正解は「たかなわ」です！';
+   let explain = document.createElement('p');
+    explain.classList.add("underline");
+    explain.innerText = '正解！';
     resultDivided.appendChild(explain);
+
+    console.log(explain);
+
+    let explain2 = document.createElement('p');
+    explain2.innerText = '正解は「たかなわ」です';
+    resultDivided.appendChild(explain2);
+
+
+    resultDivided.classList.add('result-border');
+    
+
     buttonTrue.classList.add("cannot-click");
     buttonFalse1.classList.add("cannot-click");
     buttonFalse2.classList.add("cannot-click");
+    
 }
 buttonTrue.addEventListener('click', correctAnswer);
 
-
-//buttonFalse2.addEventListener('click', falseAnswer);
-
-//不正解の時の結果
-/*function falseAnswer(){
-    buttonFalse1.style.color = '#ffffff'; 
-    buttonFalse1.classList.add('background-color2');
-    buttonFalse2.style.color = '#ffffff';
-    buttonFalse2.classList.add('background-color2');
-*/
 
 //不正解の時の結果
 buttonFalse1.onclick = function (){
@@ -47,6 +45,7 @@ buttonFalse1.onclick = function (){
    
 //不正解エリアの作成
 
+    
     let failure1 = document.createElement('span');
     failure1.className = 'underline2';
     failure1.innerText = '不正解（泣）';
@@ -54,8 +53,6 @@ buttonFalse1.onclick = function (){
     buttonTrue.classList.add("cannot-click");
     buttonFalse1.classList.add("cannot-click");
     buttonFalse2.classList.add("cannot-click");
-
-    console.log(failure);
     
 }
 //不正解の時の結果
